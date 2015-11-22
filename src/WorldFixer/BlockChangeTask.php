7eb57319f4f10f478cd9c4e7d28ec81d0ea72a71
Creeperface01 <honzaxp01@gmail.com>
2015-11-22 14:06:09 +0100

    WorldFixer

---
+++ src/WorldFixer/BlockChangeTask.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WorldFixer;
+
+
+use pocketmine\scheduler\AsyncTask;
+
+class BlockChangeTask extends AsyncTask{
+
+    public function __construct(){
+
+    }
+
+    public function onRun(){
+
+    }
+}
