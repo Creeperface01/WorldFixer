0b73e8ad8716783bdb6b1a37bd193ef8cccbc0af
Creeperface01 <honzaxp01@gmail.com>
2015-12-27 08:28:49 +0100

    improved commands

--- src/WorldFixer/WorldFixer.php
+++ src/WorldFixer/WorldFixer.php
@@ -13,13 +13,13 @@ use pocketmine\utils\Cache;
 use pocketmine\level\Level;
 
 class WorldFixer extends PluginBase implements Listener{
-    
+
     public $selectors = [];
-    
+
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
-    
+
     public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
 
         if(!$sender instanceof Player){
@@ -27,6 +27,10 @@ class WorldFixer extends PluginBase implements Listener{
         }
 
         if(strtolower($cmd->getName()) == "wf"){
+            if(!isset($args[0])){
+                $sender->sendMessage(TextFormat::RED."Use /wf help for help");
+                return;
+            }
             switch(strtolower($args[0])){
                 case "fixcolor":
                     if(!$sender->hasPermission("wf.command.fixcolor") && !$sender->isOp()){
@@ -35,6 +39,7 @@ class WorldFixer extends PluginBase implements Listener{
                     }
 
                     if(!$this->isPosSet($sender)){
+                        $sender->sendMessage(TextFormat::RED."You must select both positions first!");
                         return;
                     }
 
@@ -42,10 +47,10 @@ class WorldFixer extends PluginBase implements Listener{
                     list($x2, $y2, $z2, $level2) = explode(':', $this->selectors[strtolower($sender->getName())]['pos2']);
 
                     if($level1 !== $level2){
-                        $sender->sendMessage(TextFormat::RED."Selected positions must be in the same level!");
+                        $sender->sendMessage(TextFormat::RED."Both positions must be in the same level!");
                         return;
                     }
-                    
+
                     $level = $level1;
 
                     $this->fix($level, $x1, $y1, $z1, $x2, $y2, $z2, true, false);
@@ -58,12 +63,14 @@ class WorldFixer extends PluginBase implements Listener{
                         break;
                     }
                     if(!$this->isPosSet($sender)){
+                        $sender->sendMessage(TextFormat::RED."You must select both positions first!");
                         return;
                     }
 
                     list($x1, $y1, $z1, $level1) = explode(':', $this->selectors[strtolower($sender->getName())]['pos1']);
                     list($x2, $y2, $z2, $level2) = explode(':', $this->selectors[strtolower($sender->getName())]['pos2']);
                     if($level1 !== $level2){
+                        $sender->sendMessage(TextFormat::RED."Both positions must be in the same level!");
                         return;
                     }
 
@@ -79,12 +86,14 @@ class WorldFixer extends PluginBase implements Listener{
                         break;
                     }
                     if(!$this->isPosSet($sender)){
+                        $sender->sendMessage(TextFormat::RED."You must select both positions first!");
                         return;
                     }
 
                     list($x1, $y1, $z1, $level1) = explode(':', $this->selectors[strtolower($sender->getName())]['pos1']);
                     list($x2, $y2, $z2, $level2) = explode(':', $this->selectors[strtolower($sender->getName())]['pos2']);
                     if($level1 !== $level2){
+                        $sender->sendMessage(TextFormat::RED."Both positions must be in the same level!");
                         return;
                     }
 
@@ -106,14 +115,20 @@ class WorldFixer extends PluginBase implements Listener{
                     $this->selectors[strtolower($sender->getName())]['block'] = 1;
                     $sender->sendMessage(TextFormat::GREEN."Now select two blocks");
                     break;
+                case "help":
+                    $sender->sendMessage(TextFormat::YELLOW."> WorldFixer help <\n".TextFormat::GREEN."/wf wand ".TextFormat::GRAY."select two positions\n".TextFormat::GREEN."/wf fixslabs ".TextFormat::GRAY."Fix all slabs in the world\n".TextFormat::GREEN."/wf fixcolor ".TextFormat::GRAY."change grass color to green\n".TextFormat::GREEN."/wf fix ".TextFormat::GRAY."fix all slabs in the world and set grass color to green");
+                    break;
+                default:
+                    $sender->sendMessage(TextFormat::RED."Use /wf help for help");
+                    break;
             }
         }
     }
-    
+
     public function isSelector(Player $p){
         return isset($this->selectors[strtolower($p->getName())]['block']) && $this->selectors[strtolower($p->getName())]['block'] > 0;
     }
-    
+
     public function onBlockBreak(BlockBreakEvent $e){
         $p = $e->getPlayer();
         $b = $e->getBlock();
@@ -134,7 +149,7 @@ class WorldFixer extends PluginBase implements Listener{
             }
         }
     }
-    
+
     public function isPosSet(Player $p){
         if(isset($this->selectors[strtolower($p->getName())]['pos1']) && isset($this->selectors[strtolower($p->getName())]['pos2'])){
             return true;
@@ -147,19 +162,19 @@ class WorldFixer extends PluginBase implements Listener{
             return false;
         }
 
-        if($color === true && !$slabs){
+        /*if($color === true && !$slabs){
             for($x = min($x1, $x2); $x < max($x1, $x2); $x++){
                 for($z = min($z1, $z2); $z < max($z1, $z2); $z++){
                     $level->setBiomeColor($x, $z, 108, 151, 47);
                 }
             }
             return true;
-        }
+        }*/
 
         for($x = min($x1, $x2); $x < max($x1, $x2); $x++){
             for($y = min($y1, $y2); $y < max($y1, $y2); $y++) {
                 for ($z = min($z1, $z2); $z < max($z1, $z2); $z++) {
-                    if($level->getBlockIdAt($x, $y, $z) === 126){
+                    if($slabs && $level->getBlockIdAt($x, $y, $z) === 126){
                         $level->setBlockIdAt($x, $y, $z, 158);
                     }
                     if($color){
